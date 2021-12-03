b2c425acca7a6ecf01012103a815b8456cc4e9fd
jeongsam3659 <57051148+jeongsam3659@users.noreply.github.com>
2021-12-03 15:23:50 +0900

    30분요약 html
    1만시간의 법칙 강의
    30분요약 clone

--- js/1mTime.js
+++ js/1mTime.js
@@ -11,8 +11,8 @@ const loading = document.querySelector(".result_loading");
 function calculator() {
     // input
     const fieldValue = document.querySelector("#field_value");
-    let timeValue = document.querySelector("#time_value");
-    let timeValue_int = Number(timeValue.value);
+    const timeValue = document.querySelector("#time_value");
+    const timeValue_int = Number(timeValue.value);
 
     // result
     const fieldResult = document.querySelector(".field_result");
@@ -20,11 +20,11 @@ function calculator() {
 
     // 빈칸일시.
     if (fieldValue.value == "") {
-        alert("입력되어있지 않습니다.");
+        alert("분야 입력되어있지 않습니다.");
         fieldValue.focus();
         return false;
     } else if (timeValue.value == "") {
-        alert("숫자 입력되어있지 않습니다.");
+        alert("시간 입력되어있지 않습니다.");
         timeValue.focus;
         return false;
     } else if (timeValue_int > 24) {
@@ -36,12 +36,12 @@ function calculator() {
     loading.style.display = "flex";
 
     setTimeout(function () {
-        loading.style.display = "none";
-        result.style.display = "flex";
         fieldResult.innerText = fieldValue.value;
         timeResult.innerText = parseInt(10000 / timeValue_int, 10);
         // 텍스트를 넣는다.
         //  parseInt 소수점 x , 나눈값을 10진수로 표시
+        loading.style.display = "none";
+        result.style.display = "flex";
     }, 1800);
     // 1.8s
 }
